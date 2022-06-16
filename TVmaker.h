02b9df30a297b9df8351d9b9f6cacc52bf09fc59
Tree Kim <kkj3877@gmail.com>
2022-06-16 17:32:45 +0900

    LEA256_SHA2_TV commit

--- TVmaker.h
+++ TVmaker.h
@@ -4,10 +4,14 @@
 #include <stdlib.h>
 
 void copyLine(char * buffer, unsigned int BUFFER_SIZE, FILE * src, FILE * dest);
+void copyLineWithoutBuffer(FILE * src, FILE * dest);
 void passLine(FILE * src);
 
 unsigned int txtToReq_NonPass(const char * txtFileName, const char * reqFileName, const int * passNonPass, unsigned int lineNumOfSet, unsigned int numOfLine, unsigned int BUFFER_SIZE);
 unsigned int txtToReq_Pass(const char * txtFileName, const char * reqFileName, const int * passNonPass, unsigned int lineNumOfSet, unsigned int numOfLine, unsigned int BUFFER_SIZE, unsigned int numOfPassLine);
+unsigned int txtToReq_PassWithCopy(const char * txtFileName, const char * reqFileName, const int * passNonPass, unsigned int lineNumOfSet, unsigned int numOfLine, unsigned int BUFFER_SIZE, unsigned int numOfPassLine);
 
-
+unsigned int makeTV_ARIA(void);
+unsigned int makeTV_LEA(void);
+unsigned int makeTV_SHA2(void);
 #endif
